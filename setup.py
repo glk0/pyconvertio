61d911845f140c8d5a5a1b158e93865f4787c7e9
glk0 <hotkatz32@gmail.com>
2023-01-10 22:35:54 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
+
+setup (
+    name='pyconvertio',
+    version='0.0.0',
+    description='Lightweight Python wrapper for the Convertio  API',
+    author='Kossi GLOKPOR',
+    license='Unlicense',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License"
+    ],
+    install_requires=['requests', 'validators'],
+    python_requires='>=3.8',
+    include_package_data=True
+)
